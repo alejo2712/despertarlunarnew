78d43e9fb7bd9385c207e2b7051db7c43cae65bb
Ale Piedrabuena <alejopiedrabuena@gmail.com>
2022-06-08 21:27:13 -0300

    Ejercicio del curso JS p/ clase Programación con funciones

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,18 @@ function incrementClick() {
     console.log(counterVal);
 }
 
+function decrementClick() {
+    if (!counterVal == 0) {
+        updateDisplay(--counterVal);
+        console.log(counterVal);
+    }
+}
+
 function resetCounter() {
     counterVal = 0;
     updateDisplay(counterVal);
 }
 
 function updateDisplay(val) {
-    document.getElementById("counter-label").innerHTML = val;
+    document.getElementById("number1").value = val;
 }
